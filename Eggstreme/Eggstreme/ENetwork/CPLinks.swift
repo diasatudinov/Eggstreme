8e825ca11c8bb5ebc6c5ffb892ffac132b9c96ba
Dias <dias.atudinov@gmail.com>
2025-11-19 23:02:14 +0500

    new files

---
+++ Eggstreme/Eggstreme/ENetwork/CPLinks.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+class CPLinks {
+    
+    static let shared = CPLinks()
+    
+    static let winStarData = "https://solarbloomx.com/client"
+    
+    @AppStorage("finalUrl") var finalURL: URL?
+    
+    
+}
